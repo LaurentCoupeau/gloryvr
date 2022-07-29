184a7bbc62f755f3fb41478ada911ba199debabf
Cthulhu <joly534@gmail.com>
2022-07-29 12:08:27 +0200

    update animation of the sas

--- js/sas-functions.js
+++ js/sas-functions.js
@@ -1,79 +1,101 @@
 // video pub de glorydev
 
 
+
+
 AFRAME.registerComponent('open-sas', {
+
 	init: function () {
+
         let yState = -1;
         let zState = 0;
+
         let stateSas = false;
+        let countUp = 0;
+        let countDown = 20;
+
         let sas = document.querySelector('#sas');
         let controlSas = document.querySelector('#control-sas');
+
         let sasSound = document.createElement('audio');
         sasSound.src='assets/sounds/sas.mp3';
 
-        controlSas.addEventListener('click', function() {
+        function moveDoor(){
             sasSound.play();
 
-            switch(stateSas) {
-
-                case false:
-
-                    setInterval(() => {
-
-                        if (yState <= 1) {
-
-                            yState += 0.1;
-                            zState = -0.05;
-                            sas.setAttribute('position', {
-                                x: 0,
-                                y:yState,
-                                z:zState
-                            });
-
-                        } else {
-
-                            yState = 1;
-                            sas.setAttribute('position', {
-                                x:0,
-                                y:yState,
-                                z:zState
-                            });
-                            stateSas = true
-                        };
-
-                    }, 60);
-                    break;
+            if (stateSas) {                    
                 
-                case true:
+                let myInterval = setInterval(() => {
+                    if ( countDown < 20 ) {
+                        yState -= 0.1;
+                        zState = -0.02;
+                        sas.setAttribute('position', {
+                            x: 0,
+                            y:yState,
+                            z:zState
+                        });
+                        countUp -= 1;
+                        countDown +=1;
+                        console.log('ça descend') ;
+                        
+                    } else {
+                        clearInterval(myInterval);
+                        sas.setAttribute('position', {
+                            x: 0,
+                            y:-1,
+                            z:0
+                        });
+                        console.log('ça a fini de descendre');
+                    }
+                }, 60);                    
 
-                    setInterval(() => {
+            } else {
 
-                        if (yState >= -1) {
+                let myInterval = setInterval(() => {
 
-                            yState -= 0.1;
-                            sas.setAttribute('position', {
-                                x: 0,
-                                y:yState,
-                                z:-0.05
-                            });
+                    if (countUp < 20) {
 
-                        } else {
+                        yState += 0.1;
+                        zState = -0.02;
+                        sas.setAttribute('position', {
+                            x: 0,
+                            y:yState,
+                            z:zState
+                        });
+                        countUp += 1;
+                        countDown -=1;
+                        console.log('ça monte') ;
 
-                            yState = -1;
-                            sas.setAttribute('position', {
-                                x: 0,
-                                y:yState,
-                                z:0
-                            });
-                            stateSas = false
-                        };
+                    }else {
 
+                        clearInterval(myInterval);
+                        sas.setAttribute('position', {
+                            x: 0,
+                            y:1,
+                            z:-0.02
+                        });
+                        console.log('ça a fini de monter')  
+                    }
+                                        
                     }, 60);
-                    break;
 
-            }          
+                }
 
+        }
+
+        controlSas.addEventListener('click', function() {
+
+            if (stateSas) {
+                moveDoor();
+                stateSas = false;
+            } else {
+                moveDoor();
+                stateSas = true;
+            }
+
+            
         });
-    }	
+
+    }    
 	
 })
