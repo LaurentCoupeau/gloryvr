f1bbd94abc8b717cf93f824be4bac7862d4be637
Cthulhu <joly534@gmail.com>
2022-08-29 11:16:34 +0200

    update
    update saber

--- js/saber.js
+++ js/saber.js
@@ -1,16 +1,66 @@
 
-        let stateSaber = true;
+let stateSaber = false;
+let stateLaser = false;
 
 AFRAME.registerComponent('open-saber', {
+
 
 	init: function () {
 
         let button = document.getElementById('button-saber');
         let laser = document.getElementById('laserOfSaber');
+        let sabOn = document.getElementById('sabon');
+        let sabOff = document.getElementById('saboff');
 
-        button.addEventListener('click', {
+        let speed = 0.01;
+        let yPosClose= 0;
+        let yPosOpen= 0.6;
+        let yHeightClose=0;
+        let yHeightOpen= 0.3;
 
-            
+
+        button.addEventListener('click', function() {
+
+                if (stateSaber) {
+
+                        sabOff.play();
+
+                        laser.setAttribute('geometry', 'height', 0);
+                        laser.setAttribute('position', {
+                                x:0,
+                                y:0,
+                                z:0
+                        });
+
+                        stateSaber = false;
+                        
+                } else {
+
+                        sabOn.play();
+
+
+                                let timing =  setInterval(() => {
+
+                                        if (stateLaser == false) {
+                                        
+                                        laser.setAttribute('geometry', 'height', yHeightClose += speed);
+                                        laser.setAttribute('position', {
+                                                x:0,
+                                                y:0.3,
+                                                z:0
+                                        });
+
+                                        } else {
+
+                                                clearInterval(timing);
+                                        }
+                                }, 60);
+                         
+
+
+                        stateSaber = true;
+
+                }
 
         })
 
