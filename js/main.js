c56ce0095c8dc97c1f4ea11494516e23d1361431
Cthulhu <joly534@gmail.com>
2022-09-13 10:20:19 +0200

    update
    allow the click on vr button

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let world = document.getElementById('scene');
-let fisrtScreen = document.getElementById('first-screen');
-
-function launch() {
-
-    world.style.display = 'inline';
-    fisrtScreen.style.display= 'none';
-
-}
